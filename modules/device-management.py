ef2568c58ad57fc71f071b707d18df9d22203c45
Pink-Menace <101428947+Pink-Menace@users.noreply.github.com>
2022-03-15 10:01:54 -0400

    Update device-management.py

--- modules/device-management.py
+++ modules/device-management.py
@@ -27,9 +27,8 @@ cprint(colored("""Welcome to the Device Management Menu:
 01: Delete File
 02: Delete Directory
 03: Create Folder
-04: ES File Manager Pro Apk
-05: Start/Stop SSH Service
-06: Back To The Main Menu
+04: Start/Stop SSH Service
+05: Back To The Main Menu
 """, colors.yellow, attrs = attributes.bold))
 selection = input(colored(">>> Please Select An Option: ", colors.magenta,  attrs = attributes.bold))
 if selection == '1':
@@ -42,11 +41,6 @@ elif selection == '3':
   dir_name = input(colored("Directory Name: ", colors.magenta,  attrs = attributes.bold))
   os.system('mkdir ' + dir_name)
 elif selection == '4':
-  url = "https://github.com/J0k3r-Squ4d/es-file-manager-pro/raw/main/ES_File_Explorer_v4.2.4.3.2_MOD_apkmody.io.apk"
-  cprint(colored("ES-File-Manager-Pro.apk is downloading to directory: /sdcard. Please Wait...", colors.yellow, attrs = attributes.bold))
-  urlretrieve(url, "/sdcard/ES-File-Manager-Pro.apk")
-  cprint(colored("ES-File-Manager-Pro.apk finished downloading. It is ready to install.", colors.yellow, attrs = attributes.bold))
-elif selection == '5':
   os.system("sshd")
-elif selection == '6':
+elif selection == '5':
   os.system("python main.py")
